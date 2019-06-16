a6244601fbfb7aba0fbfd613db8655ea5236cc17
t0yohei <inuneko690121@gmail.com>
2019-06-16 19:32:57 +0900

    index/homeのviewを作成

---
+++ app/javascript/components/HeaderView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+<style></style>
+<script>
+export default {
+  props: {
+    title: "",
+    description: ""
+  }
+};
+</script>
---
+++ app/javascript/components/home/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="homeIndex">
+    <header-view v-bind:title="title" v-bind:description="description"></header-view>
+    <!-- <contents :contents="contents"></contents> -->
+  </div>
+</template>
+
+<script>
+import HeaderView from "../HeaderView.vue";
+export default {
+  components: {
+    "header-view": HeaderView
+  },
+  data: function() {
+    return {
+      title: "",
+      description: "",
+      contents: {}
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
