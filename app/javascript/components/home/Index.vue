403aa436601e4a75982b91f17fbfeb2ea6a435e2
t0yohei <inuneko690121@gmail.com>
2019-06-29 16:22:53 +0900

    Index.vueを作成してhome/indexの表示を実装

---
+++ app/javascript/components/home/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <table class="contents-table">
+      <tr>
+        <th>名前</th>
+        <th>リンク</th>
+      </tr>
+      <tr v-for="outer_link in contents.outer_links" v-bind:key="outer_link.name">
+        <td>{{ outer_link.name }}</td>
+        <td>
+          <a v-bind:href="outer_link.url">{{ outer_link.text }}</a>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.contents-table {
+  border: 1px solid gray;
+  margin: 10px;
+}
+.contents-table th,
+.contents-table td {
+  border: 1px solid gray;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: () => ""
+    },
+    description: {
+      type: String,
+      default: () => ""
+    },
+    contents: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
