e4d4abd3f54cd8cddca5d10946da870c3b99e92a
t0yohei <inuneko690121@gmail.com>
2019-06-29 16:50:49 +0900

    integerLiteralDescriptionsの初期表示を作成

---
+++ app/javascript/components/integerLiteralDescriptions/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header-view v-bind:title="title" v-bind:description="description"></header-view>
+    <contents v-bind:contents="contents"></contents>
+  </div>
+</template>
+
+<script>
+import HeaderView from "../HeaderView.vue";
+import Contents from "./Contents.vue";
+
+export default {
+  components: {
+    "header-view": HeaderView,
+    contents: Contents
+  },
+  data: function() {
+    return {
+      title: "title",
+      description: "description",
+      contents: []
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
