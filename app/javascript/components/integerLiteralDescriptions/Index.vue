356b7e68447062959964544502a241ad61d99078
t0yohei <inuneko690121@gmail.com>
2019-07-04 10:04:23 +0900

    axios でのデータ取得を実装

--- app/javascript/components/integerLiteralDescriptions/Index.vue
+++ app/javascript/components/integerLiteralDescriptions/Index.vue
@@ -8,18 +8,35 @@
 <script>
 import HeaderView from "../HeaderView.vue";
 import Contents from "./Contents.vue";
+import Axios from "axios";
 
 export default {
   components: {
     "header-view": HeaderView,
     contents: Contents
   },
+
   data: function() {
     return {
       title: "title",
       description: "description",
       contents: []
     };
+  },
+
+  created: function() {
+    this.updateContents();
+  },
+
+  methods: {
+    updateContents() {
+      Axios.get("/api/v1/integer_literal_descriptions/index.json").then(
+        response => {
+          const responseData = response.data;
+          console.log(responseData);
+        }
+      );
+    }
   }
 };
 </script>
